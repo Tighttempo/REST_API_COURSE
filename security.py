d79245d1837bbf367e2fd89885cae3c44045a4a2
Tighttempo <riseanddeliver@protonmail.com>
2022-02-21 01:44:28 +0100

    First commit, added REST API to access items, stores, and user authentication

---
+++ security.py
@@ -0,0 +1,14 @@
+# JWT info @ https://blog.teclado.com/simple-jwt-authentication-with-flask-jwt/
+
+import hmac
+from models.user import UserModel
+
+def authenticate(username, password):
+    user = UserModel.find_by_username(username)
+    if user and hmac.compare_digest(user.password, password):
+        return user
+
+
+def identity(payload):
+    user_id = payload['identity']
+    return UserModel.find_by_id(user_id)
