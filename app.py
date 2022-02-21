15b92156bbb496c40099878a5fef21f5e068c7ec
Tighttempo <riseanddeliver@protonmail.com>
2022-02-21 22:57:19 +0100

    hanged the DB Url

--- app.py
+++ app.py
@@ -10,8 +10,7 @@ from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 
 app = Flask(__name__)
-db_url = os.environ.get('DATABASE_URL')
-app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace("://", "ql://", 1)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'jose'
 api = Api(app)
